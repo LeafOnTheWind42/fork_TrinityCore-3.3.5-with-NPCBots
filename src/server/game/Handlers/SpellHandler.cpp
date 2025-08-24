8572f0ea8b64721afc98af214606c9907896a498
LeafOnTheWind42 <leafonthewind042@gmail.com>
2025-08-24 17:09:59 -0400

    changes needed for transmog vendor approach

--- src/server/game/Handlers/SpellHandler.cpp
+++ src/server/game/Handlers/SpellHandler.cpp
@@ -693,17 +693,18 @@ void WorldSession::HandleMirrorImageDataRequest(WorldPacket& recvData)
                     data << uint32(0);
                     continue;
                 }
-                // start npcbots change
-                uint32 display_id = bot->GetBotAI()->GetEquipDisplayId(slot);
-                if (display_id)
-                    data << uint32(display_id);
-                // end npcbots change
-                /* this is what I had before npcbots change 
+                // npcbots change start, conflicted with mine, reinstituting mine
+                // uint32 display_id = bot->GetBotAI()->GetEquipDisplayId(slot);
+                // if (display_id)
+                //     data << uint32(display_id);
+                // npcbots change end
+                // zzTransmogCompatibility start 
+                // this is what I had before npcbots change 
                 // ToDo - figure out if I still need something from below to get my changes to work
                 Item const* item = bot->GetBotEquips(slot);
                 if (item)
                     data << GetNPCBotTransmogDisplayId(item); // TransmogDisplayVendor/NPCBot compatibility - show transmog on NPCBot
-                */
+                // zzTransmogCompatibility end
                 else
                 {
                     //don't allow to go naked
